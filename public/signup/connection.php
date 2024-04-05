2cfd376449b87baa309973c7f72fe5c5def1aa01
EsraaAwadEid <159037870+EsraaAwadEid@users.noreply.github.com>
2024-04-05 23:24:17 +0300

    Update connection.php

--- public/signup/connection.php
+++ public/signup/connection.php
@@ -3,7 +3,7 @@
 	 $host = "sql212.infinityfree.com";
 	 $username = "if0_35679654";
 	 $password = "ZD45o4bHwfXNl";
-	 $db_name = "if0_35679654_XXX";
+	 $db_name = "if0_35679654_cspsyco";
 		
 	 $conn=new mysqli($host,$username,$password,$db_name,3306);
 	
